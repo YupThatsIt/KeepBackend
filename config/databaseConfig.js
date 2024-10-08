e8a133faded5e53f3b17e9822e46f3b250f807be
SAI P <94220352+YupThatsIt@users.noreply.github.com>
2024-10-08 23:46:30 +0700

    <Update> change database url

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async() => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/keepDB"); // use await to wait for connection
+        // await mongoose.connect("mongodb://127.0.0.1:27017/keepDB"); // use await to wait for connection
+        await mongoose.connect("mongodb+srv://saiprapamonton:kL0kluANJo1PfOKH@cluster1.wvgpg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"); // use await to wait for connection
         console.log("DB Connected");
     }
     catch(err) {
