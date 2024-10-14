c004e945fde65b7641a41178bfa012282a96b5a6
SAI P <94220352+YupThatsIt@users.noreply.github.com>
2024-10-14 18:55:16 +0700

    <Feature> add new util function to generate member number

---
+++ utils/newMemberIdGenerator.js
@@ -0,0 +1,20 @@
+function misingNumInSeq(source, min = 0, max = source.length - 1){
+    if(min >= max){
+        return min + 1;
+    }
+    let pivot = Math.floor((min + max)/2);
+    // problem is in right side. Only look at right sub array
+    if(source[pivot] === pivot + 1){
+        return misingNumInSeq(source, pivot + 1, max);
+    } else {
+        return misingNumInSeq(source, min , pivot);
+    }
+};
+
+const newMemberNumber = (seq) => {
+    const result = misingNumInSeq(seq);
+    if (result === Math.max(...seq)) return seq.length + 1;
+    else return result;
+}
+
+module.exports = newMemberNumber;
